416dd245aad671e19ae684f9c7d6ea23063e614e
nguyentan <nguyentan0842010534@gmail.com>
2025-03-03 22:44:56 +0700

    TWO WAY BINDING

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderLayoutComponent } from "./shared/header-layout/header-layout.component";
+import { FormsModule} from '@angular/forms';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeaderLayoutComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-basic';
+  nameBtn ='Click me!';
+
+  clickMessage ='';
+
+  bindingMessage ='';
+
+  updateField(): void{
+    console.log('Hello Tan');
+  }
+
+  handleClickMe(): void{
+    this.clickMessage = 'Click me Hello Tan';
+  }
 }
