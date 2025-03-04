093870c2917293571ebc593bf68eac4f2acf9ea2
nguyentan <nguyentan0842010534@gmail.com>
2025-03-04 10:57:52 +0700

    Dynamic Route

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderLayoutComponent } from "../shared/header-layout/header-layout.component";
 import { FormsModule} from '@angular/forms';
 import { CurrencyPipe } from '../shared/pipes/CurrencyPipe.pipe';
@@ -8,7 +8,7 @@ import { NgClass, NgFor, NgIf } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, HeaderLayoutComponent,FormsModule, CurrencyPipe, UpperCasePipe, NgFor, NgIf ],
+  imports: [RouterOutlet,FormsModule, CurrencyPipe, UpperCasePipe, NgFor, NgIf,RouterLink ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -22,10 +22,10 @@ export class HomeComponent {
   isVisible=false;
 
   products = [
-    { name: 'Blue De Chanel', price: 400000, image: 'assets/images/bleu_de_chanel.jpg'},
-    { name: 'Dior Sauvage', price: 500000, image: 'assets/images/dior_sauvage.jpg'},
-    { name: 'Labo', price: 600000, image: 'assets/images/labo.jpg'},
-    { name: 'Terre Hermes', price: 700000, image: 'assets/images/terre_d_hermes.jpg'},
+    {id: 1, name: 'Blue De Chanel', price: 400000, image: 'assets/images/bleu_de_chanel.jpg'},
+    {id: 2, name: 'Dior Sauvage', price: 500000, image: 'assets/images/dior_sauvage.jpg'},
+    {id: 3, name: 'Labo', price: 600000, image: 'assets/images/labo.jpg'},
+    {id: 4, name: 'Terre Hermes', price: 700000, image: 'assets/images/terre_d_hermes.jpg'},
   ];
 
   updateField(): void{
